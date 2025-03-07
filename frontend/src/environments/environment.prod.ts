7974d0f709a20fbfacd94e412811f0697804a057
Nidhun <hello@example.com>
2025-03-07 15:01:30 -0500

    updated Frontend Deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     // Replace <EC2_PUBLIC_IP> with your production backend IP or domain
-    apiUrl: `http://${{ secrets.EC2_HOST }}/api`
+    apiUrl: 'http://your-production-backend-domain-or-ip/api'
+
   };
   
