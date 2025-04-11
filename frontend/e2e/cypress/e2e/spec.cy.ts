8568228ea100105e58f821dd2bb9cf4aa16d3a25
Nidhun <hello@example.com>
2025-04-11 02:39:40 -0400

    Update e2e

--- frontend/e2e/cypress/e2e/spec.cy.ts
+++ frontend/e2e/cypress/e2e/spec.cy.ts
@@ -1,5 +1,16 @@
-describe('My First Test', () => {
-  it('Visits the Example Website', () => {
-    cy.visit('/#/login')
+describe('Login and Dashboard Test', () => {
+  it('Should log in and show dashboard', () => {
+    cy.visit('/#/login') // baseUrl should be set!
+
+    // Fill login form
+    cy.get('#uname').type('nidhunofficial@gmail.com')
+    cy.get('#pass').type('fm123')
+    cy.get('#loginbtn').click()
+
+    // Wait for dashboard route to load
+    cy.url().should('include', '/dashboard')
+
+    // Verify something on the dashboard
+    cy.contains('Dashboard') //
   })
 })
