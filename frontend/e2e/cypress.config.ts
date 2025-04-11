1b2020b9cca7203d28d0f1bb069c25dcb322340b
Nidhun <hello@example.com>
2025-04-11 02:21:49 -0400

    Update Cypress

--- frontend/e2e/cypress.config.ts
+++ frontend/e2e/cypress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://filemanage-frontend.s3-website-us-east-1.amazonaws.com',
+    specPattern: 'cypress/e2e/*.cy.ts',
+    supportFile: 'cypress/support/e2e.ts',
   },
 });
