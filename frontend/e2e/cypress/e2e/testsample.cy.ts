be01eb72419b9db54ae0a8c6be7d7d759255e626
Nidhun <hello@example.com>
2025-04-11 23:20:54 -0400

    Update local e2e

--- frontend/e2e/cypress/e2e/testsample.cy.ts
+++ frontend/e2e/cypress/e2e/testsample.cy.ts
@@ -1,6 +1,15 @@
+import 'cypress-xpath';
+
 describe('Roles Page E2E Test', () => {
     beforeEach(() => {
       cy.visit('/#/login');
       cy.url().should('include', '/login');
+    });
+
+    it('Should show Roles title in header', () => {
+        cy.xpath('/html/body/app-root/app-root/app-login/div/div/div[2]/div/div/div/div/div/div[2]/form/div[1]/div/input').type('test@test.com');
+        cy.get('/html/body/app-root/app-root/app-login/div/div/div[2]/div/div/div/div/div/div[2]/form/div[2]/div/input').type('test');
+        cy.get('#loginbtn').click();
+        cy.url().should('include', '/dashboard');
     });
 })
